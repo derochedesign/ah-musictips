6598961123a8423178846e479880b251b3716fcf
Aaron <aaron@deroche.design>
2021-02-13 20:54:19 -0500

    fixed repeat check and mobile

--- js/main.js
+++ js/main.js
@@ -30,8 +30,8 @@ const colourSets = [
     {x:"rgb(194,84,246)", y:"rgb(48,54,123)"},
     {x:"rgb(255,8,97)", y:"rgb(214,242,140)"},
     {x:"rgb(212,58,130)", y:"rgb(150,75,12)"},
-    {x:"rgb(194,84,246)", y:"rgb(48,54,123)"},
-    {x:"rgb(194,84,246)", y:"rgb(48,54,123)"}
+    {x:"rgb(194,4,46)", y:"rgb(79,156,98)"},
+    {x:"rgb(240,184,56)", y:"rgb(210,54,180)"}
 ]
 
 let prevState = [
@@ -63,23 +63,27 @@ const genState = _ => {
     console.log(currTip.id);
     
     if (result.length == 0) {
-        let gradX = colourSets[Math.floor(Math.random() * Math.floor(colourSets.length))].x;
-        let gradY = colourSets[Math.floor(Math.random() * Math.floor(colourSets.length))].y;
         
-        root.style.setProperty('--colour-grad-x', gradX);
-        root.style.setProperty('--colour-grad-y', gradY);
+        let randGrad = Math.floor(Math.random() * Math.floor(colourSets.length));
+        
+        while (randGrad === prevState[prevState.length - 1].grad) {
+            randGrad = Math.floor(Math.random() * Math.floor(colourSets.length));
+            console.log("DUP COL");
+        }
+            root.style.setProperty('--colour-grad-x', colourSets[randGrad].x);
+            root.style.setProperty('--colour-grad-y', colourSets[randGrad].y);
         
         //newTip
         tipField.innerText = currTip.tip;
         
         if (prevState.length === memMax) {
-            prevState = prevState.splice(0,1)
+            prevState.splice(0,1);
             
         }
         prevState.push({
             id: currTip.id,
             tip: currTip.tip,
-            grad:0
+            grad: randGrad
         });
     }
     else {
